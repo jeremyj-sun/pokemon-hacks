f60c3b6c7e809bcb9ae521b891084855a496a64c
Jeremy Sun <jeremy.sun@imaginecommunications.com>
2022-09-02 08:29:33 -0400

    update sleep sequence handler

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-LOG_FOLDER = "keylogs"
+LOG_FOLDER = "keylogs" # Folder to hold all keylogs
+IGNORE_SLEEP_SEQUENCE = "ignore_sleep\n" # Special sequence to begin ignoring sleeps
+RESUME_SLEEP_SEQUENCE = "resume_sleep\n" # Special sequence to resume sleeps
